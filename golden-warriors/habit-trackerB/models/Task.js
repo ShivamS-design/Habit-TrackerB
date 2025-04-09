27de7ecdf6dd58914418c8d7e31961059d2c431c
ShivamS-design <shivamnadar05@gmail.com>
2025-04-09 18:11:39 -0500

    Delete golden-warriors/habit-trackerB directory

--- golden-warriors/habit-trackerB/models/Task.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-const TaskSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  name: { type: String, required: true },
-  completed: { type: Boolean, default: false },
-});
-export default mongoose.model('Task', TaskSchema);
--- golden-warriors/habit-trackerB/routes/leaderboardRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getLeaderboard } from '../controllers/leaderboardController.js';
-
-const router = express.Router();
-
-// Get top users by XP
-router.get('/', getLeaderboard);
-
-export default router;
